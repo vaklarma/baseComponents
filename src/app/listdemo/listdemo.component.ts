df9b10a53f7d266d6e1adf09fbc3fce72d236655
biro.andrasgabor <biro.andrasgabor.regist@gmail.com>
2018-09-21 23:01:49 +0200

    + ng forban button elhelyezése + elem törlése a listbóé filter - el

--- src/app/listdemo/listdemo.component.ts
+++ src/app/listdemo/listdemo.component.ts
@@ -1,17 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+export class EventModel {
+
+  id: number;
+  name: string;
+
+
+}
 
 @Component({
   selector: 'app-listdemo',
   templateUrl: './listdemo.component.html',
   styleUrls: ['./listdemo.component.css']
 })
-export class ListdemoComponent  {
+
+
+export class ListdemoComponent {
+
+
+  eventlist: EventModel[];
 
   constructor() {
+    this.eventlist = [
+      {
+        id: 1,
+        name: 'akarmi'
+      },
+      {
+        id: 2,
+        name: 'ez is valami'
+      },
+      {
+        id: 3,
+        name: 'harmadik'
+      }
+    ];
 
 
-    console.log ('listdemo');
+  }ł
+
+  deleteItemFromList(id: number) {
+
+    this.eventlist = this.eventlist.filter((ev: EventModel) => ev.id !== id);
   }
-
 
 }
