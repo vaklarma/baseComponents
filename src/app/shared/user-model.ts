9b0ba25c7ae67b564944aa29a94fb8126e578980
biro.andrasgabor <biro.andrasgabor.regist@gmail.com>
2018-10-10 22:45:51 +0200

    login html javítása bootstrap stílusokkal user model létrehozása

--- src/app/shared/user-model.ts
+++ src/app/shared/user-model.ts
@@ -1,2 +1,19 @@
 export class UserModel {
+  id: number;
+  name: string;
+  email: string;
+
+constructor (param?: UserModel) {
+  if (param) {
+    Object.assign(this, param);
+  }
+}
+  static get exampleUser(): UserModel {
+    return {
+      id: 0,
+      name: 'Makkmarci Béla',
+      email: 'makkmarci@gmai.com'
+    };
+  }
+
 }
